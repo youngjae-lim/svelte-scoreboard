71f361d5522ab201f2a5db03963b606d29e4e8da
Youngjae <limyoungjae@gmail.com>
2020-11-04 22:56:18 -0500

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,47 @@
+<script>
+  import Navbar from './components/Navbar.svelte';
+  import Player from './components/Player.svelte';
+  import AddPlayer from './components/AddPlayer.svelte';
+
+  let players = [
+    {
+      name: 'John Doe',
+      points: 53,
+    },
+    {
+      name: 'Sam Smith',
+      points: 45,
+    },
+    {
+      name: 'Sara Wilson',
+      points: 34,
+    },
+  ];
+
+  const addPlayer = (e) => {
+    const newPlayer = e.detail;
+    players = [...players, newPlayer];
+  };
+
+  const removePlayer = (e) => {
+    players = players.filter((player) => player.name !== e.detail);
+  };
+</script>
+
+<style>
+</style>
+
+<Navbar />
+<div class="container">
+  <AddPlayer on:addplayer={addPlayer} />
+  {#if players.length === 0}
+    <p>No Players</p>
+  {:else}
+    {#each players as player}
+      <Player
+        name={player.name}
+        points={player.points}
+        on:removeplayer={removePlayer} />
+    {/each}
+  {/if}
+</div>
